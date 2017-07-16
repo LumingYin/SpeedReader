752b0deeb5822553b913839d217c6e70218687d1
Luming Yin <vista980622@gmail.com>
2017-07-16 03:38:54 -0400

    Complete sidebar UI

---
+++ SpeedReader/SRPreferencesViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SRPreferencesViewController.swift
+//  SpeedReader
+//
+//  Created by Bright on 7/16/17.
+//  Copyright © 2017 Kay Yin. All rights reserved.
+//
+
+import Cocoa
+
+class SRPreferencesViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
---
+++ SpeedReader/SRSplitViewController.swift
@@ -0,0 +1,18 @@
+//
+//  SRSplitViewController.swift
+//  SpeedReader
+//
+//  Created by Bright on 7/16/17.
+//  Copyright © 2017 Kay Yin. All rights reserved.
+//
+
+import Cocoa
+
+class SRSplitViewController: NSSplitViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
