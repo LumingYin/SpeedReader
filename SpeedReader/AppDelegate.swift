5bf7cd09961d70fb19f4298f232b1519699debb4
Luming Yin <vista980622@gmail.com>
2017-07-16 16:49:19 -0400

    Auto close app when all window closes.

--- SpeedReader/AppDelegate.swift
+++ SpeedReader/AppDelegate.swift
@@ -117,6 +117,10 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         // If we got here, it is time to quit.
         return .terminateNow
     }
+    
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
 
 
 }
