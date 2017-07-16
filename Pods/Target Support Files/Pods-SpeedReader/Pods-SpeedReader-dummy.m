a4e5d1357ddab7040797719500c59f397ccdbebc
Luming Yin <vista980622@gmail.com>
2017-07-16 18:55:14 -0400

    Preliminary support for parsing HTML pages.

---
+++ Pods/Target Support Files/Pods-SpeedReader/Pods-SpeedReader-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_SpeedReader : NSObject
+@end
+@implementation PodsDummy_Pods_SpeedReader
+@end
--- SpeedReader/SRImportWebViewController.swift
+++ SpeedReader/SRImportWebViewController.swift
@@ -7,42 +7,100 @@
 //
 
 import Cocoa
+import Kanna
 
-class SRImportWebViewController: NSViewController {
+class SRImportWebViewController: NSViewController, NSTextFieldDelegate {
     @IBOutlet weak var urlLabel: NSTextField!
-
+    @IBOutlet weak var titleLabel: NSTextField!
+    var dotDotDotTimer: Timer?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         getClipboardURL()
+        urlLabel.delegate = self
+    }
+    
+    override func controlTextDidChange(_ obj: Notification) {
+        parseWebpageAsync()
     }
     
     
     @IBAction func cancelPressed(_ sender: NSButton) {
+        (NSApp.delegate as? AppDelegate)?.persistentContainer.viewContext.reset()
         self.view.window?.sheetParent?.endSheet(self.view.window!, returnCode: NSModalResponseCancel)
     }
     
     @IBAction func importPressed(_ sender: NSButton) {
-        parseWebpageAsync()
+        (NSApp.delegate as? AppDelegate)?.saveAction(nil)
         self.view.window?.sheetParent?.endSheet(self.view.window!, returnCode: NSModalResponseOK)
-
     }
     
     func parseWebpageAsync() {
-        if (urlLabel.stringValue.contains("http")) {
-            URLSession.shared.dataTask(with: URL(string: urlLabel.stringValue)!) { (data: Data?, response: URLResponse?, error: Error?) in
-                if error != nil {
-                    print("\(String(describing: error))")
-                } else {
-                    if (data != nil) {
-                        let html:String = String(data: data!, encoding: String.Encoding.utf8)!
-                        
-                        if let doc = HTML(html: html, encoding: .utf8) {
-                            print(doc.title)
-                        }
-                        print(html)
-                    }
+        dotDotDotTimer?.invalidate()
+        self.titleLabel.stringValue = "Loading..."
+        dotDotDotTimer = Timer.scheduledTimer(withTimeInterval: 0.4, repeats: true, block: { (timer) in
+            if self.titleLabel.stringValue.contains("...") {
+                self.titleLabel.stringValue = "Loading"
+            } else if self.titleLabel.stringValue.contains("..") {
+                self.titleLabel.stringValue = "Loading..."
+            } else if self.titleLabel.stringValue.contains(".") {
+                self.titleLabel.stringValue = "Loading.."
+            } else {
+                self.titleLabel.stringValue = "Loading."
+            }
+        })
+        guard let url = URL(string: urlLabel.stringValue) else {
+            self.titleLabel.stringValue = "Invalid Web URL"
+            self.dotDotDotTimer?.invalidate()
+            return
+        }
+        URLSession.shared.dataTask(with: url) { (data: Data?, response: URLResponse?, error: Error?) in
+            if error != nil {
+                print("\(String(describing: error))")
+                self.setLabelUnavailable()
+            } else {
+                guard let data = data,
+                    let html:String = String(data: data, encoding: String.Encoding.utf8),
+                    let doc = HTML(html: html, encoding: .utf8),
+                    let title = doc.title else {
+                    self.setLabelUnavailable()
+                    return
                 }
-                }.resume()
+                var articleContent = ""
+                for link in doc.xpath("//p | //h1 | //h2") {
+                    guard let text = link.text else {
+                        return
+                    }
+                    articleContent = articleContent + text + "\n"
+//
+//                    print(link.text)
+//                    print(link["href"])
+                }
+//                for td in body {
+//                }
+                DispatchQueue.main.async {
+                    guard let context = (NSApp.delegate as? AppDelegate)?.persistentContainer.viewContext else {
+                        return
+                    }
+                    let newArticle: Article = Article(context: context)
+                    newArticle.typeOfArticle = 2
+                    newArticle.webPageUrl = self.urlLabel.stringValue
+                    newArticle.lastUpdated = Date.init() as NSDate
+                    newArticle.webPageTitle = title
+                    newArticle.content = "\(title)\n\(articleContent)"
+                    
+                    self.titleLabel.stringValue = title
+                    self.dotDotDotTimer?.invalidate()
+                }
+
+            }
+            }.resume()
+    }
+    
+    func setLabelUnavailable() {
+        DispatchQueue.main.async {
+            self.titleLabel.stringValue = "Unavailable"
+            self.dotDotDotTimer?.invalidate()
         }
     }
     
@@ -54,6 +112,7 @@ class SRImportWebViewController: NSViewController {
                         if let url = item.string(forType: type) {
                             if url.hasPrefix("http://") || url.hasPrefix("https://") {
                                 urlLabel.stringValue = url
+                                parseWebpageAsync()
                             }
                         }
                     }
